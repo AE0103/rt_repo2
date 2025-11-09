471fd2cdcd28f13fff1bac20aa8e78f31bc1eb01
AE0103 <andrewestvz@gmail.com>
2025-11-09 17:10:41 -0500

    Add files via upload

--- Assets/Scripts/EnemyThree.cs
+++ Assets/Scripts/EnemyThree.cs
@@ -13,7 +13,7 @@ public class EnemyThree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, -2, 0) * Time.deltaTime * 3f);
+        transform.Translate(new Vector3(2, -2, 0) * Time.deltaTime * 3f);
         if (transform.position.y < -6.5f)
         {
             Destroy(this.gameObject);
