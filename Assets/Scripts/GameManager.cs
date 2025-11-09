586708b22df4346b3a6753259c33330fb6baee77
AE0103 <andrewestvz@gmail.com>
2025-11-09 17:07:58 -0500

    Add files via upload

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,13 @@ public class GameManager : MonoBehaviour
 {
 
     public GameObject enemyOnePrefab;
+    public GameObject enemyThreePrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("CreateEnemyOne", 1, 2);
+        InvokeRepeating("CreateEnemyThree", 3, 4);
     }
 
     // Update is called once per frame
@@ -22,5 +24,11 @@ public class GameManager : MonoBehaviour
     void CreateEnemyOne()
     {
         Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyThree()
+    {
+        Instantiate(enemyThreePrefab, new Vector3(Random.Range(-8f, 8f), 5.5f, 0), Quaternion.identity);
+
     }
 }
